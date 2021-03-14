7a40aa950791afd95b2128ee4da06b8731755758
Joachim <djoow@pm.me>
2021-03-14 16:13:44 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# dump_MySQL_databases
+**Table des matières**
+- [Script dump base de données MySQL](#script-dump-base-de-donn-es-mysql)
+  * [Introduction](#introduction)
+  * [Utilisation & Arguments](#utilisation---arguments)
+  * [Amélioration à venir](#am-lioration---venir)
+  * [Codes erreurs](#codes-erreurs)
+
+# Script dump base de données MySQL
+*Script pour un projet école*
+
+## Introduction
+
+Script permettant de faire des dumps de base de données MySQL.
+
+## Utilisation & Arguments
+
+`-a`: Permet à l'utilisateur de dump toutes les bases de données MySQL s'appuie sur l'argument `mysqldump --all-databases`
+`-c`: Permet de créer une archive des bases de données récemment dump
+`-h`: Montre l'aide du script
+`-n`: Dump une BDD en particulier, nécéssite un argument exemple `./dump_dbb.sh -n DBNAME`
+`-o`: Dump uniquement les bases de données créé par l'administrateur.
+`-z`: Dump toutes les bdd et les archives en `tar.xz`
+
+- La variable `MYSQL_ROOT_PASS` doit être complété afin que le script fonctionne
+
+En prérequis, il faut ajouter les droits d'execution sur le script.
+
+```bash
+chmod u+x dump_bdd.sh
+```
+
+
+
+## Amélioration à venir
+
+Suppression de la variable `MYSQL_ROOT_PASS` pour un input utilisateur qui demande le mot de passe qui sera stocké dans un fichier `my.cnf`.
+
+## Codes erreurs
+- `exit 1`: Script n'est pas exécute en tant que root
+- `exit 2`: Binaire mysqldump manquant
+- `exit 4`: On ne peut pas écrire dans le dossier de destination de backup
+- `exit 5`: Aucun argument trouvé en utilisant l'option `-n`
+- `exit 6`: Erreur sur le dump
+- `exit 7`: N'arrive pas à créer le dossier archive  sur le partage NFS s'il n'existe pas.
+- `exit 8`: L'archivage a déjà été fait à ce jour
+- `exit 9`: Le dossier d'archive est vide, il ne peut pas créer une archive vide
+- `exit 255`: SIGTERM détecté
---
+++ dump_bdd.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+# =======================================================
+# NAME: dump_bdd.sh
+# AUTHOR: Joachim
+# DATE: 21/09/19
+# MODIFICATION: 31/01/20
+# License: UNLICENSE
+# VERSION 1
+# COMMENTS: Script permettant de faire des dumps de base de données et de les compresser
+# =======================================================
+
+# déclaration des variables pour coloriser les outputs
+declare -r YL='\e[33m' # Yellow
+declare -r RD='\e[31m' # Red
+declare -r GN='\e[92m' # Green
+declare -r RST="\e[0m" # Reset
+
+# Variable Serveur
+SRV_HOSTNAME=$(hostname)
+declare -r SRV_TIME=$(date +"%d_%m_%Y")
+declare -r SRV_LOG="/var/log/dump.log"
+# Variable pour Mysql
+declare -r MYSQL_BIN='/usr/bin/mysqldump'
+declare -r MYSQL_ROOT_LOGIN='root'
+declare -r MYSQL_ROOT_PASS=''
+declare -r MYSQL_HOST='localhost'
+declare -r MYSQL_DUMP_FOLDER="/backup/db_dump"
+declare -r MYSQL_CMD="-u$MYSQL_ROOT_LOGIN -p$MYSQL_ROOT_PASS -h$MYSQL_HOST"
+declare -r INDIVIDUAL_DUMP=$2
+declare -r MYSQL_EXT='.sql'
+
+trap exit_script SIGINT
+if [[ $EUID -ne '0' ]]; then
+  echo -e "$RD You must be root ${RST}"
+  exit 1
+fi
+
+if ! command -v $MYSQL_BIN >/dev/null 2>&1; then
+  echo  -e "$RD mysqldump not found ${RST}"
+  exit 2
+fi
+
+# On test l'écriture dans notre point de montage
+if ! touch $MYSQL_DUMP_FOLDER'/testfile'; then
+  echo -e "$RD Permission denied ${RST}"
+  exit 4
+else
+  rm "${MYSQL_DUMP_FOLDER}/testfile"
+fi
+
+function dump_all_user_created_db() {
+  # On récupère en variable le nom des bdd et on enlève du scope les bdd systèmes
+DB=$(mysql -u${MYSQL_ROOT_LOGIN} -p${MYSQL_ROOT_PASS} -e 'show databases;'| grep -Ev "(Database|information_schema|mysql|performance_schema|sys)")
+  for dbname in $DB
+  do
+    ${MYSQL_BIN} ${MYSQL_CMD} --databases $dbname > ${MYSQL_DUMP_FOLDER}/${SRV_TIME}'_'${SRV_HOSTNAME}'_'${dbname}${MYSQL_EXT} || echo -e "$RD DUMP KO " >> $SRV_LOG
+    echo -e "$GR $dbname has been dump ! ${RST}" | awk '{ print strftime("%H:%M:%S"), $0; }' >> $SRV_LOG
+  done
+}
+function dump_individual() {
+
+  # On vérifie qu'on a un argument en $2
+  if [[ $INDIVIDUAL_DUMP == "" ]]; then
+    echo -e "$RD no argument found"
+    exit 5
+  else
+    # Dump de la base de données
+    ${MYSQL_BIN} ${MYSQL_CMD} --databases "$INDIVIDUAL_DUMP" > ${MYSQL_DUMP_FOLDER}/${SRV_TIME}'_'${SRV_HOSTNAME}'_'${INDIVIDUAL_DUMP}${MYSQL_EXT}
+  fi
+
+  if [[ $? -ne '0' ]]; then
+    echo -e "$RD Dump KO ! "${RST} >> $SRV_LOG
+    exit 6
+  else
+    # Envoie d'un rapport dans le fichier de log
+    echo -e "$GR The database: $INDIVIDUAL_DUMP has been dump "${RST} | awk '{ print strftime("%H:%M:%S"), $0; }' >> $SRV_LOG
+  fi
+}
+
+function dump_all() {
+  DB=$(mysql -u${MYSQL_ROOT_LOGIN} -p${MYSQL_ROOT_PASS} -e 'show databases;')
+  echo -e "$YL MYSQLDUMP has been launched !!"
+    for dbname in $DB
+      do
+        ${MYSQL_BIN} ${MYSQL_CMD} --all-databases > ${MYSQL_DUMP_FOLDER}/${SRV_TIME}'_'${SRV_HOSTNAME}'_all'${MYSQL_EXT}
+        echo -e "$GR $dbname has been dump ! ${RST} " | awk '{ print strftime("%H:%M:%S"), $0; }' > $SRV_LOG || echo -e "$RD Dump KO "${RST} >> $SRV_LOG
+      done
+}
+
+function dump_compression () {
+
+  # Fonction permettant de créer une archive d'un dump
+  # Je définis mes variables
+  save_folder='/backup/dbarchive/'
+  n='0'
+  nf=$(ls $MYSQL_DUMP_FOLDER | wc -l ) # nf=number of file
+  archive_name="${SRV_HOSTNAME}_archive_db_${SRV_TIME}.tar.xz"
+  dump_file="${save_folder}${archive_name}"
+
+  # Je verifie que mon dossier de stockage d'archive existe
+    if [[ ! -d $save_folder ]]; then
+      mkdir $save_folder || echo -e "$RD KO  Can't create folder ${RST}"
+    fi
+  # Je verifie que l'archive n'existe pas déjà
+  if [[ -f "$dump_file" ]]; then
+    echo -e "$YL Archive already exist, try tomorrow ! ${RST}"
+    exit 8
+  fi
+
+  # Création de l'archive si le dossier n'est pas vide  et je change les permission de l'archive
+  if [[ $nf -eq $n ]] ; then
+    echo -e "$RD KO Folder is empty !! "${RST} >> $SRV_LOG
+    exit 9
+  else
+    echo -e "$YL Archive has been launched !${RST}"
+    for file in $MYSQL_DUMP_FOLDER
+    do
+      tar -cJf $dump_file $file && chmod 400 $dump_file
+    done
+  fi
+
+  if [[ $? -ne '0' ]]; then
+    echo -e "$RD Archive KO" 2>&1 | tee  $SRV_LOG
+  else
+    echo -e "$GN Archive OK" 2>&1 | tee  $SRV_LOG
+    /bin/rm -rf $MYSQL_DUMP_FOLDER
+  fi
+}
+
+function exit_script() {
+  # fonction permettant de gerer les ctrl +c
+  echo -e " - $red SIGINT detected (CTRL + C) Exit ! "
+  read -n 1 -s -r -p "Press any key to continue ! "
+  exit 255
+}
+function usage() {
+  #affiche la page d'aide au commande
+  echo
+  echo "+------------------------------------------------------------------------------------------------------------------------------------------------------------------+"
+  echo "Options:"
+  echo "   -a                                  Allow user to dump all databases in mysql"
+  echo "   -c,                                 Allow user to create archive from dumped files"
+  echo "   -h                                  Show script usage "
+  echo "   -n,                                 Allow user to dump only one database need an argument to work"
+  echo "   -o,                                 Allow user to dump all databases created by the user"
+  echo "   -z,                                 Allow user to dump all DB and create tar from it "
+  echo "+------------------------------------------------------------------------------------------------------------------------------------------------------------------+"
+
+  exit 0
+}
+
+if [[ $1 == "" ]]; then
+  usage
+  exit 0
+fi
+
+while getopts 'achn:oz' opt
+do
+  case $opt in
+    a) dump_all;;
+    c) dump_compression;;
+    h) usage;;
+    n) dump_individual $OPTARG;;
+    o) dump_all_user_created_db;;
+    z) dump_all;dump_compression;;
+    *) echo "show usage $0 [-h]" >&2
+       exit 100;;
+  esac
+done
